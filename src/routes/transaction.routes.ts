98d79a0855bccb027cc41cb689de7d120103f98c
cbufano <cbufano@kapatron.com.br>
2021-01-05 23:36:25 -0300

    desafio fundamentos node js

---
+++ src/routes/transaction.routes.ts
@@ -0,0 +1,42 @@
+import { Router } from 'express';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+
+const transactionRouter = Router();
+
+const transactionsRepository = new TransactionsRepository();
+
+transactionRouter.get('/', (request, response) => {
+  try {
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({
+      transactions,
+      balance,
+    });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+transactionRouter.post('/', (request, response) => {
+  try {
+    const { title, value, type } = request.body;
+    const CreateTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+    const transaction = CreateTransaction.execute({
+      title,
+      value,
+      type,
+    });
+    // console.log(transaction);
+    return response.status(200).json(transaction);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default transactionRouter;
